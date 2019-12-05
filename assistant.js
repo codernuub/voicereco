ad27ea00ca804c7cb4b75109affd853464215dce
codernuub <sardarbhai6@gmail.com>
2019-12-06 00:36:47 +0530

    fix bug

---
+++ assistant.js
@@ -0,0 +1,62 @@
+const btn = document.getElementById("start");
+const reply = document.getElementById("reply");
+const recoError = document.getElementById("recoError");
+const speechError = document.getElementById("speechError");
+
+const rep = ['hi', 'ha mei or btao', 'jao padhai kro', 'apna kaam krna'];
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+btn.textContent = "activate";
+
+
+recognition.onstart = function () {
+    btn.textContent = "Listening...";
+}
+
+recognition.onresult = function (event) {
+
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    btn.textContent = "activate";
+    reply.textContent = transcript;
+    bolo(transcript);
+
+}
+
+recognition.onerror = function (event) {
+   recoError.textContent = event.error;
+}
+
+btn.addEventListener('click', () => {
+
+    recognition.start(); 
+
+});
+function bolo(message) {
+
+    const speech = new SpeechSynthesisUtterance();
+
+    speech.onerror = function (event) {
+        speechError.textContent = event.error;
+    }
+
+    if (message.includes("hello")) {
+    
+        const reply = rep[Math.floor(Math.random() * rep.length)];
+        console.log(reply);
+        speech.text = reply;
+
+    }else{
+        speech.text = "kiya bol rahe ho humko samajh nhi araha hai";
+        reply.textContent = speech.text;
+    }
+   
+    speech.volume = 2;
+    speech.rate = 0.7;
+    speech.pitch = 0.8;
+
+    window.speechSynthesis.speak(speech);
+}
+
